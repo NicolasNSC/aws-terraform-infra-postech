ab94da6e79866ed77766da13e6360aea6e79ea9e
Nicolas Castanha <nicolascastanha2002@gmail.com>
2025-03-18 20:40:29 -0300

    testing provider without vars

--- infra-tech-challenge/provider.tf
+++ infra-tech-challenge/provider.tf
@@ -9,16 +9,10 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-variable "AWS_REGION" {}
-variable "AWS_SESSION_TOKEN" {}
+
 
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  region = var.AWS_REGION
-  token = var.AWS_SESSION_TOKEN
+  
   assume_role {
     role_arn = "arn:aws:iam::923490220058:role/LabRole"
   }
