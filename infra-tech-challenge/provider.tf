a021ca8c415545b587c7ee4772a308305089ebf7
Nicolas Castanha <nicolascastanha2002@gmail.com>
2025-03-17 22:44:29 -0300

    adjust

--- infra-tech-challenge/provider.tf
+++ infra-tech-challenge/provider.tf
@@ -15,7 +15,7 @@ variable "AWS_REGION" {}
 
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region = var.AWS_REGION
   assume_role {
     role_arn = "arn:aws:iam::923490220058:role/LabRole"
