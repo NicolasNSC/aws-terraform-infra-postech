abe89530958538362e124a398bac88ff34c8dd5f
Nicolas Castanha <nicolascastanha2002@gmail.com>
2025-03-18 20:33:56 -0300

    adding token on provider

--- infra-tech-challenge/provider.tf
+++ infra-tech-challenge/provider.tf
@@ -12,11 +12,13 @@ terraform {
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
 variable "AWS_REGION" {}
+variable "AWS_SESSION_TOKEN" {}
 
 provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   region = var.AWS_REGION
+  token = var.AWS_SESSION_TOKEN
   assume_role {
     role_arn = "arn:aws:iam::923490220058:role/LabRole"
   }
